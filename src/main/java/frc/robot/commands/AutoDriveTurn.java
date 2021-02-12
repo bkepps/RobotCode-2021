cc4fffc182306681ea2b9f3de810edc2614c03a8
Ben Kepps <benskepp@gmail.com>
2021-02-12 14:49:19 -0600

    added autoDriveTurn

--- src/main/java/frc/robot/commands/AutoDriveTurn.java
+++ src/main/java/frc/robot/commands/AutoDriveTurn.java
@@ -3,24 +3,26 @@
 // the WPILib BSD license file in the root directory of this project.
 
 package frc.robot.commands;
-
-import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.Constants.driveTrain;
 import frc.robot.subsystems.MyDriveTrain;
 
-public class AutoDrive extends CommandBase {
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
+public class AutoDriveTurn extends CommandBase {
+  
   MyDriveTrain locDriveTrain;
   Double locSpeed = 0.0;
+  Double locRSpeed = 0.0;
   Double initialPosition = 0.0;
   Double locDistance = 0.0;
-
-  /** Creates a new AutoDrive. */
-  public AutoDrive(MyDriveTrain driveTrain, double speed, double distance) {
+  
+  /** Creates a new AutoDriveTurn. */
+  public AutoDriveTurn(MyDriveTrain driveTrain, double speed, double rSpeed, double distance) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
     locDriveTrain = driveTrain;
     locSpeed = speed;
+    locRSpeed = rSpeed;
     locDistance = distance;
   }
 
@@ -28,20 +30,20 @@ public class AutoDrive extends CommandBase {
   @Override
   public void initialize() {
     initialPosition = Math.abs(locDriveTrain.getEncoderPosition());
-    
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    locDriveTrain.drive(locSpeed, 0.0);
+    locDriveTrain.drive(locSpeed, locRSpeed);
   }
 
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
     locSpeed = 0.0;
-    locDriveTrain.drive(locSpeed, 0.0);
+    locRSpeed = 0.0;
+    locDriveTrain.drive(locSpeed, locRSpeed);
   }
 
   // Returns true when the command should end.
