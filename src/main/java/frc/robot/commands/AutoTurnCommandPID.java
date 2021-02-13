5cc3b155334c229e520bc717cfdfa84ba4cd1868
Ben Kepps <benskepp@gmail.com>
2021-02-13 14:42:41 -0600

    Added comments

--- src/main/java/frc/robot/commands/AutoTurnCommandPID.java
+++ src/main/java/frc/robot/commands/AutoTurnCommandPID.java
@@ -9,7 +9,6 @@ package frc.robot.commands;
 import com.kauailabs.navx.frc.AHRS;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
-import frc.robot.Constants;
 import frc.robot.Constants.driveTrain;
 import frc.robot.subsystems.MyDriveTrain;
 import edu.wpi.first.wpilibj.DriverStation;
@@ -24,13 +23,16 @@ public class AutoTurnCommandPID extends CommandBase {
   Double initialAngle = 0.0;
   Double locAngle = 0.0;        //angle to rotate to in degrees
   
+  // NavX class thing
   AHRS ahrs;
 
-  // tolerance for error in PID
+  // tolerance for error in PID -- the closer this is to 0 the longer turning will take
   static final double kToleranceDegrees = 2.0f;
   // Creates a PIDController with gains kP, kI, and kD
   PIDController pid = new PIDController(driveTrain.kRotP, driveTrain.kRotI, driveTrain.kRotD);
 
+  //Rotate to angle "angle" at speed "rSpeed"
+  //uses a PID loop to hopefully make it more accurate
   public AutoTurnCommandPID(MyDriveTrain driveTrain, double rSpeed, double angle) {
     // Use addRequirements() here to declare subsystem dependencies.
     addRequirements(driveTrain);
@@ -53,7 +55,10 @@ public class AutoTurnCommandPID extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
+    //initialize NavX
     ahrs.reset();
+
+    //initialize PID loop
     pid.setSetpoint(locAngle);
     pid.setTolerance(kToleranceDegrees);
     pid.enableContinuousInput(-180, 180);
@@ -62,12 +67,16 @@ public class AutoTurnCommandPID extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+    //rotate at speed given by the PID loop
     locDriveTrain.drive(0.0, pid.calculate(ahrs.getAngle()));
   }
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    //stop any motion
+    locDriveTrain.drive(0.0, 0.0);
+  }
 
   // Returns true when the command should end.
   @Override
